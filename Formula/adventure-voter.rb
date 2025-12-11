5ce713e88f1e08fd4e554480b6cbfed8dbe8f3c3
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2025-12-11 15:52:44 +0100

    added new version
    Signed-off-by: Gergely Brautigam <182850+Skarlso@users.noreply.github.com>

--- Formula/adventure-voter.rb
+++ Formula/adventure-voter.rb
@@ -1,31 +1,31 @@
 class AdventureVoter < Formula
   desc "Interactive presentation system where the audience votes on what happens next"
   homepage "https://github.com/Skarlso/adventure-voter"
-  version "0.0.3"
+  version "0.0.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Skarlso/adventure-voter/releases/download/v0.0.3/adventure_darwin_arm64.tar.gz"
-      sha256 "516681a06699b2f8f172aa55a21977da595536a01f22f9bd572c4b408f5ea2ab"
+      url "https://github.com/Skarlso/adventure-voter/releases/download/v0.0.4/adventure_darwin_arm64.tar.gz"
+      sha256 "5cf162e836fb38a3ee2632db23f04b74965071b595168ca26a7efd9429a2beac"
     else
-      url "https://github.com/Skarlso/adventure-voter/releases/download/v0.0.3/adventure_darwin_amd64.tar.gz"
-      sha256 "2dd5b44011ef4ea965a7b4c12a25e9c49bd40b6aa8f3ed866f5b65be28aec2a8"
+      url "https://github.com/Skarlso/adventure-voter/releases/download/v0.0.4/adventure_darwin_amd64.tar.gz"
+      sha256 "e2eb71595553814fa7a367b528cd86a21876cc3deb6d1d1bdd030580de645ede"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/Skarlso/adventure-voter/releases/download/v0.0.3/adventure_linux_arm64.tar.gz"
-        sha256 "bdaed0359686594523e7c2461459acd0b488d152d6e65d35ab10bbb3871a28d1"
+        url "https://github.com/Skarlso/adventure-voter/releases/download/v0.0.4/adventure_linux_arm64.tar.gz"
+        sha256 "664db631158c52d2629938094a2a4bd358d007419397ad6f0331166bb0cf5cfe"
       else
-        url "https://github.com/Skarlso/adventure-voter/releases/download/v0.0.3/adventure_linux_armv7.tar.gz"
-        sha256 "e790d2c143fb597a30381fad9db12affc369e1476417e382a82b343779313109"
+        url "https://github.com/Skarlso/adventure-voter/releases/download/v0.0.4/adventure_linux_armv7.tar.gz"
+        sha256 "2b5ff2924285041344626842d38526deb62f0b2f6fb8c1b7a5fa3ceb1309b4a4"
       end
     else
-      url "https://github.com/Skarlso/adventure-voter/releases/download/v0.0.3/adventure_linux_amd64.tar.gz"
-      sha256 "a3aa9d948ef060ca3753b68a52d3e98d51cce07608878da725bce778d2f44bb5"
+      url "https://github.com/Skarlso/adventure-voter/releases/download/v0.0.4/adventure_linux_amd64.tar.gz"
+      sha256 "997eaf69982d4362e112facd7baf80de70d80ffc629ddf7cd3bafa00d0e9af32"
     end
   end
 
